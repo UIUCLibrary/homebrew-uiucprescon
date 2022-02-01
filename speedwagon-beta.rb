994b7f5cf2b4d6608b9073693c8a06b06a43b4cf
henry <henryborchers@yahoo.com>
2022-02-01 13:54:07 -0600

    update catalina hash

--- speedwagon-beta.rb
+++ speedwagon-beta.rb
@@ -14,7 +14,7 @@ class SpeedwagonBeta < Formula # rubocop:disable Metrics/ClassLength
 
   bottle do
     root_url "https://jenkins.library.illinois.edu/nexus/repository/homebrew-bottles/beta/"
-    sha256 cellar: :any, catalina: "a751045c669839dae6cfee8e992f7816f98710149fed4fed6f3a758f7dd56498"
+    sha256 cellar: :any, catalina: "1131910f8cbf2e1648b439af093f9bb48bbeefed7b874034746ffa6b496e525d"
   end
 
   depends_on "cmake" => :build
