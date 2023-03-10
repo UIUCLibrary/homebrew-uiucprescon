1f6fdc9c3004a56ce761f989840cf9dd190462f2
henry <henryborchers@yahoo.com>
2023-03-10 09:19:36 -0600

    speedwagon-beta 0.2.0b11

--- Casks/speedwagon-beta.rb
+++ Casks/speedwagon-beta.rb
@@ -1,7 +1,7 @@
 cask "speedwagon-beta" do
-  version "0.2.0b10"
-  sha256 arm: "5b16cb6434dac166bd420e9c82f781073924ed249bdf27f2fd0c4656b9fdac07",
-         intel: "7d8b2c3c183a302d7109d7355fb5ddd067d91af31439212673069a92d5e9695c"
+  version "0.2.0b11"
+  sha256 arm: "83eb6a036588ca66511ce4597677ef073b6af52867914b4e90e54d923bc1618c",
+         intel: "5d5d5f9081be45974fbb373626b0cdae72b4e9f64dd52ba434b6a3a9810c701b"
   arch arm: "arm64",
        intel: "x86_64"
   url "https://jenkins.library.illinois.edu/nexus/repository/prescon-dist/speedwagon/speedwagon-#{version}-#{arch}.dmg",
