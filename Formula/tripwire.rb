36b4c23898c90a861a5109aa9d9b576d5e9ac9aa
Henry Borchers <henryborchers@yahoo.com>
2025-04-18 15:46:32 -0500

    tripwire 0.3.2

--- Formula/tripwire.rb
+++ Formula/tripwire.rb
@@ -6,18 +6,23 @@ class Tripwire < Formula
 
   desc "Used for helping out AV preservation"
   homepage "https://github.com/UIUCLibrary/tripwire"
-  url "https://github.com/UIUCLibrary/tripwire/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "af04e31a9479152cbd821758a6ab58a150f1b46d6ada244f316e7d6396a8e5da"
+  url "https://github.com/UIUCLibrary/tripwire/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "f8fa0e8f0eda37b0f9adfa68a670f6dbd4b557f5c16f884cd31debfe967507a9"
   license "NCSA"
   head "https://github.com/UIUCLibrary/tripwire.git", branch: "main"
 
   bottle do
     root_url "https://nexus.library.illinois.edu/repository/homebrew-bottles/"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "634fa662c44105e50357798612902087909f89af83c348ddbce2e2316a9006ee"
-    sha256 cellar: :any_skip_relocation, sonoma:       "3cdbbdcc2df76bfd04c1cbd321171763c6f53ba4f925f1a3e8c91931015f79fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1757f8709f429e246f3901d44c34a8a47813c04439a96169b20e20fb2e4e19d5"
+    sha256 cellar: :any_skip_relocation, sonoma:       "db101c104ffea9e0fc7c06f807331b858148c3abcd135233c61e45f9fdea8eb7"
   end
 
   depends_on "python@3.13"
+
+  resource "argcomplete" do
+    url "https://files.pythonhosted.org/packages/16/0f/861e168fc813c56a78b35f3c30d91c6757d1fd185af1110f1aec784b35d0/argcomplete-3.6.2.tar.gz"
+    sha256 "d0519b1bc867f5f4f4713c41ad0aba73a4a5f007449716b16f385f2166dc6adf"
+  end
 
   resource "tqdm" do
     url "https://files.pythonhosted.org/packages/a8/4b/29b4ef32e036bb34e4ab51796dd745cdba7ed47ad142a9f4a1eb8e0c744d/tqdm-4.67.1.tar.gz"
