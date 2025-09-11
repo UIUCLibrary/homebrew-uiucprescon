9f46b7ad2ca3e8a7dc141c538b46d122dc413483
Henry Borchers <henryborchers@yahoo.com>
2025-09-11 11:48:50 -0500

    tripwire 0.3.3
    include tripwire 0.3.3 bottles for sonoma and arm64_sonoma

--- Formula/tripwire.rb
+++ Formula/tripwire.rb
@@ -6,15 +6,15 @@ class Tripwire < Formula
 
   desc "Used for helping out AV preservation"
   homepage "https://github.com/UIUCLibrary/tripwire"
-  url "https://github.com/UIUCLibrary/tripwire/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "f8fa0e8f0eda37b0f9adfa68a670f6dbd4b557f5c16f884cd31debfe967507a9"
+  url "https://github.com/UIUCLibrary/tripwire/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "5d9313a429731b07fb17599978d8fcef10361f58d770101cfd9c84e9601f21ba"
   license "NCSA"
   head "https://github.com/UIUCLibrary/tripwire.git", branch: "main"
 
   bottle do
     root_url "https://nexus.library.illinois.edu/repository/homebrew-bottles/"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1757f8709f429e246f3901d44c34a8a47813c04439a96169b20e20fb2e4e19d5"
-    sha256 cellar: :any_skip_relocation, sonoma:       "db101c104ffea9e0fc7c06f807331b858148c3abcd135233c61e45f9fdea8eb7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "ae8b44196a62b8fe08aa5200911a3f7380dc791c97746a0c61c55436973cce11"
+    sha256 cellar: :any_skip_relocation, sonoma:       "11fec424d205951c98d7fee56559b92a89c72fd4e586b281801eb210586d41a9"
   end
 
   depends_on "python@3.13"
@@ -24,9 +24,19 @@ class Tripwire < Formula
     sha256 "d0519b1bc867f5f4f4713c41ad0aba73a4a5f007449716b16f385f2166dc6adf"
   end
 
+  resource "pymediainfo" do
+    url "https://files.pythonhosted.org/packages/4d/80/80a6fb21005b81e30f6193d45cba13857df09f5d483e0551fa6fbb3aaeed/pymediainfo-7.0.1.tar.gz"
+    sha256 "0d5df59ecc615e24c56f303b8f651579c6accab7265715e5d429186d7ba21514"
+  end
+
   resource "tqdm" do
     url "https://files.pythonhosted.org/packages/a8/4b/29b4ef32e036bb34e4ab51796dd745cdba7ed47ad142a9f4a1eb8e0c744d/tqdm-4.67.1.tar.gz"
     sha256 "f8aef9c52c08c13a65f30ea34f4e5aac3fd1a34959879d7e59e63027286627f2"
+  end
+
+  resource "uiucprescon-pymediaconch" do
+    url "https://github.com/UIUCLibrary/uiucprescon.pymediaconch/releases/download/v0.1.1/uiucprescon_pymediaconch-0.1.1.tar.gz"
+    sha256 "c7b3217e59aab7d772352baa230f1628dccaf66ab8f4806840e11feab4ca55fd"
   end
 
   def install
