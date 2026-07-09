7eb35a9b0c53c32c9a47c666966972907c91dbf3
hborcher <hborcher@illinois.edu>
2026-07-09 08:30:34 -0500

    fix: speedwagon-prescon url

--- Casks/speedwagon-prescon.rb
+++ Casks/speedwagon-prescon.rb
@@ -1,10 +1,10 @@
 cask "speedwagon-prescon" do
-  version "0.3.2b8"
+  version "0.3.2.b8"
   sha256 arm: "250371b3ce9c3945e10db599ec33aeaab38dd2721f81baa5697bbfc5ee3685ee",
          intel: "16b77c24d0900721c091bb8406f929faa8154fd446bec2a4ed09d7061059f6a9"
   arch arm: "arm64",
        intel: "x86_64"
-  url "https://nexus.library.illinois.edu/repository/prescon-dist/speedwagon_uiuc/#{version}/Speedwagon%20(UIUC%20Prescon%20Edition)-0.3.2.b8-macos-#{arch}.dmg",
+  url "https://nexus.library.illinois.edu/repository/prescon-dist/speedwagon_uiuc/#{version}/Speedwagon%20(UIUC%20Prescon%20Edition)-#{version}-macos-#{arch}.dmg",
       verified: "nexus.library.illinois.edu/repository/prescon-dist/speedwagon_uiuc"
   name "Speedwagon"
   desc "Collection of tools and workflows for DS"
